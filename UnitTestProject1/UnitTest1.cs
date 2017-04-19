0aa722d686d54df445003cefad4e939e35ec3481
Joseph Adolfo Cordero Marín <Joseph Adolfo Cordero Marn>
2017-04-19 15:17:15 -0600

    Reservation Algorithm Ready

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using WebApplication2.Data;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            ReservationData rd = new ReservationData("Data Source=jocoma.database.windows.net;Initial Catalog=aurorahotel;Persist Security Info=True;User ID=jocoma;Password=jcm_12345");
+            List<int> result = rd.verifyReservation("2017/03/28", "2017/04/01");
+
+            foreach (int i in result)
+            {
+                Console.WriteLine("Id: " + i);
+            }
+        }
+    }
+}
---
+++ WebApplication2/Data/ReservationData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication2.Data
+{
+    public class ReservationData
+    {
+
+        private string connectionString;
+
+        public ReservationData(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<int> verifyReservation(string inDate, string outDate)
+        {
+            //conexion con la bd
+            SqlConnection sqlConn = new SqlConnection(this.connectionString);
+
+            //string sql
+            string sqlSelect = "select idtbroom  from tbroom where idtbroom not in (" +
+                                "select room from tbreservation where (indate = '" + inDate + "') or " +
+                                "('" + inDate + "' > indate and '" + inDate + "' < outdate) or " +
+                                "(indate > '" + inDate + "' and indate < '" + outDate + "') " +
+                                ");";
+
+            //establecer la conexion con el adaptador
+            SqlDataAdapter sqlDataAdapterProperty = new SqlDataAdapter();
+
+            //configurar el adaptador
+            sqlDataAdapterProperty.SelectCommand = new SqlCommand();
+            sqlDataAdapterProperty.SelectCommand.CommandText = sqlSelect;
+            sqlDataAdapterProperty.SelectCommand.Connection = sqlConn;
+
+            //definir el data set
+            DataSet datasetRooms = new DataSet();
+            List<int> roomsNotAvailableList = new List<int>();
+
+            //dataset para guardar los resultados de la consulta
+            sqlDataAdapterProperty.Fill(datasetRooms, "tbreservation");
+
+            //cerrar la conexion con el adaptador
+            sqlDataAdapterProperty.SelectCommand.Connection.Close();
+
+            DataRowCollection dataRowCollection = datasetRooms.Tables["tbreservation"].Rows;
+
+            foreach (DataRow currentRow in dataRowCollection)
+            {
+                int room = Int32.Parse(currentRow["idtbroom"].ToString());
+                roomsNotAvailableList.Add(room);
+            }
+
+            return roomsNotAvailableList;
+        }
+
+
+
+    }
+}
