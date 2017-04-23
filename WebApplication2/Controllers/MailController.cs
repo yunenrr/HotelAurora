0d4329bc328c8abe107e2fdef5760cda9fc528f6
Joseph Adolfo Cordero Marín <Joseph Adolfo Cordero Marn>
2017-04-22 22:44:54 -0600

    Contact Ready

---
+++ WebApplication2/Controllers/MailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class MailController : ApiController
+    {
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.ActionName("SendMail")]
+        public string PostSendMail(Mail mail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.To.Add(mail.From);
+                mailMessage.From = new MailAddress(mail.From);
+                mailMessage.Subject = mail.Subject;
+                string Body = mail.Content;
+                mailMessage.Body = Body;
+                mailMessage.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("hotelaurora2017@gmail.com", "2017hotelaurora");
+                smtp.EnableSsl = true;
+                smtp.Send(mailMessage);
+
+                return "Send";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+    }
+
+}
---
+++ WebApplication2/Models/Mail.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Mail
+    {
+        private string from;
+        private string to;
+        private string subject;
+        private string content;
+
+        public Mail()
+        {
+            this.from = "";
+            this.to = "";
+            this.subject = "";
+            this.content = "";
+        }
+
+        public Mail(string from, string to, string subject, string content)
+        {
+            this.from = from;
+            this.to = to;
+            this.subject = subject;
+            this.content = content;
+        }
+
+        public string From
+        {
+            get
+            {
+                return from;
+            }
+
+            set
+            {
+                from = value;
+            }
+        }
+
+        public string To
+        {
+            get
+            {
+                return to;
+            }
+
+            set
+            {
+                to = value;
+            }
+        }
+
+        public string Subject
+        {
+            get
+            {
+                return subject;
+            }
+
+            set
+            {
+                subject = value;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+            }
+        }
+
+    }
+}
